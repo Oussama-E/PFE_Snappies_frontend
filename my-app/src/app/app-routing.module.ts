0ea2fcef1d62a7eb881f3f57b3937823a219c38a
7Matteo <matteo.fiore@student.vinci.be>
2023-12-12 11:09:32 +0100

    refactor

--- my-app/src/app/app-routing.module.ts
+++ my-app/src/app/app-routing.module.ts
@@ -4,7 +4,7 @@ import { ArticleComponent } from './ADMIN/article/article.component';
 import { TourneeComponent } from './ADMIN/tournee/tournee.component';
 import { LoginFormComponent } from './LOGIN/login-form/login-form.component';
 import { CommandsListComponent } from './ADMIN/commands-list/commands-list.component';
-import { CreateCommandFormComponent } from './ADMIN/create-command-form/create-command-form.component';
+import { CreateCommandFormComponent } from './ADMIN/create-command-form/create-command-form/create-command-form.component';
 
 const routes: Routes = [ 
   { path: '', component: LoginFormComponent},
